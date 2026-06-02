fb2b896b2fb1ae8a85a912dceb4171e8a3a5c394
dariabelll <belavinadara7@gmail.com>
2026-06-03 00:47:41 +0300

    [read-path]: add lsm storage engine implementation

---
+++ include/storage/lsm/lsm_storage_engine.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "storage/api/storage_engine_interface.hpp"
+#include "lsmtree/lsmtree.hpp"
+
+#include <cstddef>
+#include <filesystem>
+#include <memory>
+#include <optional>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+namespace htap::storage {
+
+class LSMStorageEngine final : public IStorageEngine {
+public:
+    explicit LSMStorageEngine(std::string root_path);
+
+    void create_table(const std::string& table_name, const Schema& schema) override;
+
+    bool table_exists(const std::string& table_name) const override;
+
+    const Schema& get_table_schema(const std::string& table_name) const override;
+
+    void insert(const std::string& table_name, const Row& values) override;
+
+    virtual std::unique_ptr<ICursor> get(
+        const std::string& table_name,
+        Key key,
+        const std::vector<size_t>& projection) const override;
+
+    virtual std::unique_ptr<ICursor> scan(
+        const std::string& table_name,
+        std::optional<Key> from,
+        std::optional<Key> to,
+        const std::vector<size_t>& projection,
+        ScanOrder order = ScanOrder::Unordered) const override;
+
+private:
+    htap::lsmtree::LSMTree& get_tree(const std::string& table_name);
+    const htap::lsmtree::LSMTree& get_tree(const std::string& table_name) const;
+
+    std::string table_path(const std::string& table_name) const;
+
+private:
+    std::string root_path_;
+    std::unordered_map<std::string, std::unique_ptr<htap::lsmtree::LSMTree>> tables_;
+};
+
+}
